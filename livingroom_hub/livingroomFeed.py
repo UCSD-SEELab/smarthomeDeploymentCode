55fee990a3435bb3594cd8219515155a7f6cfbfe
Rishikanth <rishikanthc@gmail.com>
2018-11-30 02:05:19 -0800

    Added Living Room Files

---
+++ livingroom_hub/livingroomFeed.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append('../')
+import json
+
+from deployModels.HLdeployer.communicate import *
+
+def on_msg_tvplug(mosq, obj, msg):
+    datum = json.loads(str(msg.payload))
+    com.dataDict["tv_plug_inference"] = datum["data"][0]
+    print ("recieved tv plug", com.dataDict["tv_plug_inference"])
+
+with open("./config.json", 'r') as confFile:
+    conf = json.load(confFile)["test"]
+
+com = communicate(conf, [on_msg_tvplug])
+com.startListening()
+com.predictionLoop()
+
+
